20ca4eccfc1669d3d7c13ee542eaa3d1d0fe4392
Ananya Rajpurohit <84175666+Ananya296@users.noreply.github.com>
2022-01-19 19:06:38 +0530

    Add files via upload

---
+++ PC.js
@@ -0,0 +1,35 @@
+img = "";
+status = "";
+
+function preload()
+{
+    img = loadImage("PC.jpg");
+}
+
+function setup()
+{
+    canvas = createCanvas(500, 350);
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+}
+
+function modelLoaded()
+{
+  console.log("model Loaded!");
+  status = true;
+  objectDetector.detect(img, gotResults);
+}
+
+function gotResults( error , results)
+{
+  if (error)
+  {
+    console.error(error);
+  }
+
+  else
+  {
+    console.log(results);
+    objects = results;
+  }
+}
